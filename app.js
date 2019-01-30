16b4457f098aacadbbc1d02bb87d5f37b2f79848
Jonathan mead <lylyahiko@gmail.com>
2019-01-30 10:29:06 -0500

    Added Guestbook entry C part of CRUD

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 const app = express();
+const port = process.env.PORT;
 
-// var mongoose = require("mongoose");
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/nodejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    db = client.db('guestbook');
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
@@ -17,13 +29,21 @@ app.get('/guestbook', (req, res) => {
     res.sendFile(__dirname + '/views/guestbook.html');
 });
 
-app.post("/addname", (req, res) => {
+app.get('/thanks', (req, res) => {
+    res.sendFile(__dirname + '/views/thanks.html');
+});
 
+app.post("/addname", (req, res) => {
+    db.collection('guests').save(req.body, (err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('saved to database');
+        res.redirect('/thanks')
+    })
 });
 
 app.get('*', function(req, res){
     res.sendFile(__dirname + '/views/404.html');
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
